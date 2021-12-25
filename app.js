bfde3e977e3c5f09feb05e2b2874673672230989
afmssrahi <afmssrahi@gmail.com>
2021-12-25 23:14:17 +0600

    Attribute Binding

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			url: 'https://www.google.com/',
 			showBooks: true,
 			books: [
-				{ title: 'name of the wind', author: 'patrick rothfuss' },
-				{ title: 'the way of kings', author: 'brandon sardersand' },
-				{ title: 'thr final empire', author: 'brandon sardersand' },
+				{
+					title: 'name of the wind',
+					author: 'patrick rothfuss',
+					img: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/1.jpg',
+				},
+				{
+					title: 'the way of kings',
+					author: 'brandon sardersand',
+					img: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/2.jpg',
+				},
+				{
+					title: 'thr final empire',
+					author: 'brandon sardersand',
+					img: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/3.jpg',
+				},
 			],
 		};
 	},
