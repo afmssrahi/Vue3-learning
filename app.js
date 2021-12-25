c7e001476311139c6d75fa884511352dfbdb7607
afmssrahi <afmssrahi@gmail.com>
2021-12-25 22:30:26 +0600

    v-for

--- app.js
+++ app.js
@@ -2,26 +2,16 @@ const app = Vue.createApp({
 	data() {
 		return {
 			showBooks: true,
-			title: 'The Finale Empire',
-			author: 'Brandon Sanderson',
-			age: 45,
-			x: 0,
-			y: 0,
+			books: [
+				{ title: 'name of the wind', author: 'patrick rothfuss' },
+				{ title: 'the way of kings', author: 'brandon sardersand' },
+				{ title: 'thr final empire', author: 'brandon sardersand' },
+			],
 		};
 	},
 	methods: {
 		toggleShowBooks() {
 			this.showBooks = !this.showBooks;
-		},
-		handleEvents(e, data) {
-			console.log(e, e.type);
-			if (data) {
-				console.log(data);
-			}
-		},
-		handleMouseMove(e) {
-			this.x = e.offsetX;
-			this.y = e.offsetY;
 		},
 	},
 });
