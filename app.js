21090ae31cc03d17152cec32023495f249b36cb0
afmssrahi <afmssrahi@gmail.com>
2021-12-25 23:25:52 +0600

    Dynamic Classes

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ const app = Vue.createApp({
 					title: 'name of the wind',
 					author: 'patrick rothfuss',
 					img: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/1.jpg',
+					isFav: true,
 				},
 				{
 					title: 'the way of kings',
 					author: 'brandon sardersand',
 					img: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/2.jpg',
+					isFav: false,
 				},
 				{
 					title: 'thr final empire',
 					author: 'brandon sardersand',
 					img: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/3.jpg',
+					isFav: true,
 				},
 			],
 		};
